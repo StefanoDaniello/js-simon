bac83a5ecf64963613187529a02d1e0e3974ebb6
StefanoDaniello <157887560+StefanoDaniello@users.noreply.github.com>
2024-03-20 15:06:00 +0100

    add button

---
+++ js/script.js
@@ -0,0 +1,27 @@
+/*
+Descrizione:
+Visualizzare in pagina 5 numeri casuali ( tra 1 e 100) non duplicati.
+Da lì parte un timer di 30 secondi.
+Dopo i 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt() ( o meglio caselle di input).
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+Consigli del giorno:
+* Pensate prima in italiano.
+* Dividete in piccoli problemi la consegna.
+* Individuate gli elementi di cui avete bisogno per realizzare il programma.
+Buon pomeriggio e buon lavoro !!!! :muscolo:
+*/
+
+
+/*
+creaoi un array 
+stampo 5 numeri casuali non duplicati 
+e li inserisco in un box per 30 secondi
+prendi i numeri dell' utente 
+li metto in un array 
+stampo i numeri uguali nell' array dell' utente e nel primo array
+in un altro  e a seconda della lunghezza di esso uscirà lo score
+*/
+
+
+
+ 
